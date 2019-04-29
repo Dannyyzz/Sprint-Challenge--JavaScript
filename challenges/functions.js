aff0ce1b311726146057af29a2fc2553380ef985
Dannyyzz <35716281+Dannyyzz@users.noreply.github.com>
2019-04-28 20:59:34 -0400

    Edit README && Done w/ functions.js

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,18 +7,31 @@
   * In the body of the function return the callback with the two parameters that you created
 */
 
+let consume = (one, two, cb) => {
+  return cb(one, two);
+}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+function add(x, y) {
+  return x + y;
+}
 
+function multiply(x, y) {
+  return x * y;
+}
+
+function greeting(first_name, last_name) {
+  return `Hello ${first_name} ${last_name}, nice to meet you!`
+}
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2,2,add)); // 4
+console.log(consume(10,16,multiply)); // 160
+console.log(consume("Mary","Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
@@ -27,6 +40,7 @@
 
 // Explanation: 
 
+// It can reference `internal` because it is inside the other function which makes nestedfunction() nested into myFunction().
 
 const external = "I'm outside the function";
 
